debc0807b782cabe3f64a0bb975b71fab09e5db4
Roman <kyryliuk.roma@gmail.com>
2026-03-26 03:38:13 +0200

    fix: remove unused SpeedInsights component from App

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,6 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import { lazy, Suspense } from 'react'
-import { SpeedInsights } from '@vercel/speed-insights/react'
 import { useDeviceId } from '@/hooks/useDeviceId'
 import { useUIStore } from '@/store/uiStore'
 import OfflineStatusBanner from '@/components/OfflineStatusBanner'
@@ -111,7 +110,6 @@ export default function App() {
       )}
       <PWAInstallPrompt />
       <ReactQueryDevtools initialIsOpen={false} />
-      <SpeedInsights />
     </QueryClientProvider>
   )
 }
